1c6fa98c3cb38b93072a8172b77663d725c446da
Monu Singh <monu.singh9203@gmail.com>
2022-12-18 02:35:56 +0530

    Part1 assignment 3

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -6,6 +6,14 @@ app = FastAPI()
 def index():
     return {"message": "Hello World"}
     
+@app.get("/cool")
+def cool_stuff():
+    return {"message": "This is neat"}
+
+@app.post("/echo")
+def echo(message: str):
+    return {"echo": message}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
