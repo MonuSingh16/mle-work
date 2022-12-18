f42f83e0ec75fbf2cba3f1175c447377594828b9
Monu Singh <monu.singh9203@gmail.com>
2022-12-18 12:26:25 +0530

    Add app.py

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,18 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from transformers import pipeline
 
 app = FastAPI()
+pipeline = pipeline(
+    "translation_en_to_de",
+    model="model/local_translation"
+)
+
+class TextToTranslate(BaseModel):
+    input_text: str
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
-    
-@app.get("/cool")
-def cool_stuff():
-    return {"message": "This is neat"}
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
 
 @app.post("/echo")
-def echo(message: str):
-    return {"echo": message}
+def echo(text_to_translate: TextToTranslate):
+    return {"message": text_to_translate.input_text}
+
+@app.post("/translate")
+def translate(text_to_translate: TextToTranslate):
+    return {"message": pipeline(text_to_translate.input_text)}
 
 if __name__ == "__main__":
     import uvicorn
