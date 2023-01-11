deed33649958fe01f3114141229ac1cbabc97f04
Monu Singh <monu.singh9203@gmail.com>
2023-01-11 23:21:20 +0530

    Syncing branch to changes

--- assignments/week-04-data-eng-airflow/workflow-airflow/dags/walmart-ml-workflow.py
+++ assignments/week-04-data-eng-airflow/workflow-airflow/dags/walmart-ml-workflow.py
@@ -7,9 +7,9 @@ from airflow.operators.bash import BashOperator
 with DAG(
     'walmart-ml-workflow',
     default_args={
-        'owner': 'Flora Xu', # YOUR NAME HERE
+        'owner': 'Monu Singh', # YOUR NAME HERE
         'depends_on_past': False,
-        'email': ['flora@fourthbrain.ai'], # YOUR EMAIL HERE
+        'email': ['monu.singh9203@gmail.com'], # YOUR EMAIL HERE
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
@@ -25,12 +25,12 @@ with DAG(
     # BashOperator defines tasks that execute bash scripts. In this case, we run Python scripts for each task.
     get_store_data = BashOperator(
         task_id='get_store_data',
-        bash_command='python3 ~/airflow/scripts/get_store_data.py',
+        bash_command='python ~/airflow/scripts/get_store_data.py',
     )
     train = BashOperator(
         task_id='train',
         depends_on_past=False,
-        bash_command='python3 ~/airflow/scripts/train.py',
+        bash_command='python ~/airflow/scripts/train.py',
         retries=3,
     )
 
